6b7a0f55c7a4db7aa4d4fe0caf7a6bb3e391af88
MarcoPieretti <marcopieretti50@gmail.com>
2022-04-21 16:48:55 +0200

    Verso il completamento

--- ListaAppuntamenti/Appuntamenti.cs
+++ ListaAppuntamenti/Appuntamenti.cs
@@ -6,7 +6,36 @@ using System.Threading.Tasks;
 
 namespace ListaAppuntamenti
 {
-    internal class Appuntamenti
+    internal class Appuntamenti 
     {
+        public string nome { get; }
+        public string localitaAppuntamento { get; set; }
+
+        public DateTime dataAppuntamento { get; }
+
+        public Appuntamenti(string nome, string localitaAppuntamento, DateTime dataAppuntamento)
+        {
+            this.nome = nome;
+            this.localitaAppuntamento = localitaAppuntamento;
+            this.dataAppuntamento = dataAppuntamento;
+
+            if (this.dataAppuntamento <= DateTime.Now)
+            {
+                throw new InvalidDataException("La data inserita e' nel passato o oggi.");
+
+            }   
+        }
+
+        public override string ToString()
+        {
+            string rappresentazioneInStringa = "";
+
+            rappresentazioneInStringa += "---- Appuntamento ---- \n";
+            rappresentazioneInStringa += "Nome appuntamento: " + this.nome + "\n";
+            rappresentazioneInStringa += "Localita Appuntamento: " + this.localitaAppuntamento + "\n";
+            rappresentazioneInStringa += "Data e ora: " + this.dataAppuntamento + "\n";
+
+            return rappresentazioneInStringa;
+        }
     }
 }
--- ListaAppuntamenti/Program.cs
+++ ListaAppuntamenti/Program.cs
@@ -1,2 +1,39 @@
 ﻿using ListaAppuntamenti;
 
+List<Appuntamenti> listaAppuntamenti = new List<Appuntamenti>();
+
+Console.WriteLine("Quanti appuntamenti vuoi inserire in agenda? ");
+int numeroAppuntamenti = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < numeroAppuntamenti; i++)
+{
+    Console.Write("Inserisci nome: ");
+    string nomeAppuntamento = Console.ReadLine();
+
+    Console.Write("Inserisci localita: ");
+    string localita = Console.ReadLine();
+
+    try
+    {
+        Console.Write("Inserisci data e ora appuntamento: ");
+        DateTime dataOra = DateTime.Parse(Console.ReadLine());
+
+        listaAppuntamenti.Add(new Appuntamenti(nomeAppuntamento, localita, dataOra));
+        Console.WriteLine();
+    }
+    catch (InvalidDataException ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+    finally
+    {
+        Console.WriteLine("Inserisci nuova data: ");
+
+    }
+
+}
+
+    foreach (Appuntamenti appuntamento in listaAppuntamenti)
+    {
+        Console.WriteLine(appuntamento.ToString());
+    }
